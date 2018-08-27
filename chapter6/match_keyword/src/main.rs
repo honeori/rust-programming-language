dc850899999a9e7a6b790d516de3d3550ebcc8a0
honeori <ststlange@gmail.com>
2018-08-27 16:32:11 +0000

    chapter6.2: match keyword

---
+++ chapter6/match_keyword/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    use_value_in_center();
+    let _ = plus_one(Some(3));
+    let _ = plus_one(None);
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn use_value_in_center() {
+    let coin = Coin::Quarter(UsState::Alabama);
+    let value = value_in_cents(coin);
+    println!("the value is {}", value);
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
