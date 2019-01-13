e70467c0db47635bf65dba14efc4cc8ab659216c
honeori <ststlange@gmail.com>
2019-01-13 20:43:21 +0000

    chpter10: intoroduction

---
+++ chapter10/generic_data_types/src/main.rs
@@ -0,0 +1,20 @@
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+
+    println!("The largest number is {}", result)
+}
+
