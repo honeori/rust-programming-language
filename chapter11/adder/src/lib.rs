ce345bac2575e78de9a95d43d5497615dfb7c55a
honeori <ststlange@gmail.com>
2019-01-13 22:42:11 +0000

    chapter11: tests

---
+++ chapter11/adder/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    fn is_square(&self) -> bool {
+        self.length == self.width
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7};
+        let smaller = Rectangle { length: 5, width: 1};
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn test_is_square() {
+        let square = Rectangle { length: 7, width: 7};
+
+        assert!(square.is_square());
+    }
+}
